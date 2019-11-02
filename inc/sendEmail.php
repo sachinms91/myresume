24780afb6667d61fd37a73559453fc7be2ac8dca
Sachin M S <sachinms1991@gmail.com>
2019-11-02 22:51:33 +0530

    Update sendEmail.php

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,7 +1,7 @@
-﻿<?php
+﻿<!-- <?php
 
 // Replace this with your own email address
-$siteOwnersEmail = 'user@website.com';
+$siteOwnersEmail = '@website.com';
 
 
 if($_POST) {
@@ -66,4 +66,4 @@ if($_POST) {
 
 }
 
-?>
+?> -->
